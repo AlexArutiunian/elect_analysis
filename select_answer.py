a6a55539aeab0d88f5687379289d4bb7da2ff17c
AlexArutiunian <arutunanalexandr17@gmail.com>
2024-02-08 22:16:25 +0300

    add worked

--- select_answer.py
+++ select_answer.py
@@ -3,36 +3,51 @@ from bs4 import BeautifulSoup
 import os
 
 def select_answer_from_html(file_name):
-
 	with open(file_name, "r", encoding="utf-8") as f:
-		html = f.read()
+		try:
+			html = f.read()
+		except UnicodeDecodeError as e:
+			
+			with open("errors.txt", "a", encoding="utf-8") as f_er:
+				f_er.write(f"UnicodeDecodeError: {e} with {file_name} \n")
+			return []
 
 	soup = BeautifulSoup(html, "html.parser")
-
-	divs_answer = soup.find_all("div")
 
 	answers = []
 
 	for elem in soup.find_all("p"):
 		elem_text = elem.get_text()
-		if "sentiment" in elem_text:
-		    print(elem_text)
-    return answers
-    
-path = "htmls"    
+		if "sentiment" or "Sentiment" in elem_text:
+			print(elem_text)
+			answers.append(elem_text)
+
+	return answers  # вернуть ответы
+
+path = "recov"    
 jsons_path = "bunchs"    
-    
+
 for file_ in os.listdir(path):
+    print(file_)
+    if not file_.endswith(".html"):
+        continue
     answers = select_answer_from_html(path + "/" + file_)
-    with open(json_path + "/" + file_.replace(".html", ".json"), "r", encoding="utf-8") as fp:
+    with open(jsons_path + "/" + file_.replace(".html", ".json"), "r", encoding="utf-8") as fp:
         data = json.load(fp)
-    data["sentiment_claim"] = answer[0]
-    data["sentiment_evidence"] = answer[1]
-    with open(json_path + "/" + file_.replace(".html", ".json"), "w", encoding="utf-8") as fp:
+
+    try:        
+        data["sentiment_claim"] = answers[0]
+    except IndexError:
+        print("IndexError")
+        with open("errors.txt", "a", encoding="utf-8") as f_er:
+            f_er.write("IndexError with claim in " + file_ + "\n")
+        data["sentiment_claim"] = "NOTHING"
+    try:        
+        data["sentiment_evidence"] = answers[1]
+    except IndexError:
+        print("IndexError")
+        with open("errors.txt", "a", encoding="utf-8") as f_er:
+            f_er.write("IndexError with evidence in " + file_ + "\n")
+        data["sentiment_evidence"] = "NOTHING" 
+    with open(jsons_path + "/" + file_.replace(".html", ".json"), "w", encoding="utf-8") as fp:
         json.dump(data, fp, indent=2)
-            		    
-
-# print(divs_answer)
-
-    
-
