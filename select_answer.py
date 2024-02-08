d9b53bd3dff4b68eb8d99ed4442ee4577a9183c0
AlexArutiunian <arutunanalexandr17@gmail.com>
2024-02-08 13:37:05 -0600

    add func for json proccessing

--- select_answer.py
+++ select_answer.py
@@ -1,20 +1,36 @@
 import json
 from bs4 import BeautifulSoup
+import os
 
-with open("x.html", "r", encoding="utf-8") as f:
-    html = f.read()
+def select_answer_from_html(file_name):
 
-soup = BeautifulSoup(html, "html.parser")
+	with open(file_name, "r", encoding="utf-8") as f:
+		html = f.read()
 
-divs_answer = soup.find_all("div")
+	soup = BeautifulSoup(html, "html.parser")
 
-answers = []
+	divs_answer = soup.find_all("div")
 
+	answers = []
 
-for elem in soup.find_all("p"):
-    elem_text = elem.get_text()
-    if "sentiment" in elem_text:
-        print(elem_text)
+	for elem in soup.find_all("p"):
+		elem_text = elem.get_text()
+		if "sentiment" in elem_text:
+		    print(elem_text)
+    return answers
+    
+path = "htmls"    
+jsons_path = "bunchs"    
+    
+for file_ in os.listdir(path):
+    answers = select_answer_from_html(path + "/" + file_)
+    with open(json_path + "/" + file_.replace(".html", ".json"), "r", encoding="utf-8") as fp:
+        data = json.load(fp)
+    data["sentiment_claim"] = answer[0]
+    data["sentiment_evidence"] = answer[1]
+    with open(json_path + "/" + file_.replace(".html", ".json"), "w", encoding="utf-8") as fp:
+        json.dump(data, fp, indent=2)
+            		    
 
 # print(divs_answer)
 
